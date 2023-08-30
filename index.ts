14a65a2dc8b1886c7fd99912f0fa8355d08d988e
David Arenas <davejs136@gmail.com>
2023-08-30 00:48:33 -0400

    :tada: initial commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { Hono } from "hono";
+import { cors } from "hono:cors";
+import "dotenv";
+import initConnection from "./db.ts";
+
+initConnection();
+
+const PORT = Deno.env.get("PORT");
+
+const app = new Hono();
+
+app.use("*", cors());
+
+app.get("/", (c) => c.json({ message: "Alfagames API - Dave136" }));
+
+Deno.serve({ port: Number(PORT) || 3030 }, app.fetch);
