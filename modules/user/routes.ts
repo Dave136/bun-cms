87bda40f03ef4302e0453038b5dfdf976ebab3a7
David Arenas <davejs136@gmail.com>
2023-09-04 01:24:29 -0400

    refactor(user): remove register route

--- modules/user/routes.ts
+++ modules/user/routes.ts
@@ -1,20 +1,10 @@
 import { Hono } from "hono";
-import User from "./model.ts";
 
 const user = new Hono();
 
 user.post(
-  "/register",
-  async (c) => {
-    const body = await c.req.json();
-    const found = await User.findOne({ email: body.email });
-
-    if (found) {
-      return c.json({
-        message: "User already exists",
-      }, 404);
-    }
-
+  "/:id",
+  (c) => {
     return c.json({
       message: "ok",
     });
