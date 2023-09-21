6dc62cab39d63c05a4916e5acde1ddbff0c07098
David Arenas <davejs136@gmail.com>
2023-09-20 20:25:01 -0400

    feat(api): refresh token

--- src/modules/auth/routes.ts
+++ src/modules/auth/routes.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { setCookie, getCookie } from "hono/cookie";
 import * as authService from "./service.ts";
 import * as userService from "../user/services.ts";
 import httpResponse from "../../utils/http-response.ts";
@@ -7,6 +8,8 @@ import {
   InvalidCredentials,
   UserByEmailNotFound,
 } from "../user/error.ts";
+import { ExpiredTokenProvidedForRefresh } from "./error.ts";
+import { errors } from "jose";
 
 const route = new Hono();
 
@@ -18,7 +21,16 @@ route.post("/login", async (c) => {
       return httpResponse.badRequest(c, "Missing credentials");
     }
 
-    const { token } = await authService.authenticate(credentials);
+    const { token, refreshToken } = await authService.authenticate(credentials);
+
+    setCookie(c, "refreshToken", refreshToken, {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      sameSite: "Strict",
+      path: "/",
+      signingSecret: process.env.JWT_PRIVATE_KEY,
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+    });
 
     return httpResponse.ok(c, {
       token,
@@ -57,6 +69,32 @@ route.post("/register", async (c) => {
     }
 
     return httpResponse.internalServerError(c, error as string);
+  }
+});
+
+route.post("/refresh", async (c) => {
+  try {
+    const currentToken = getCookie(c, "refreshToken");
+
+    if (!currentToken) {
+      return httpResponse.badRequest(c, "Missing token");
+    }
+
+    const token = await authService.refreshToken(currentToken);
+
+    return httpResponse.ok(c, {
+      token,
+    });
+  } catch (error) {
+    if (error instanceof ExpiredTokenProvidedForRefresh) {
+      return httpResponse.badRequest(c, error.message);
+    }
+
+    if (error instanceof errors.JWSInvalid) {
+      return httpResponse.badRequest(c, error.message);
+    }
+
+    return httpResponse.internalServerError(c, error);
   }
 });
 
