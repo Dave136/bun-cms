fd497a37ea100d4a4db492039d4b2a5f0ff0c990
David Arenas <davejs136@gmail.com>
2023-09-28 00:15:59 -0400

    refactor(VideoConsoles): remove unused code

--- ui/src/lib/components/collections/video-consoles/VideoConsoles.svelte
+++ ui/src/lib/components/collections/video-consoles/VideoConsoles.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import Button from "$lib/components/ui/button/button.svelte";
   import Input from "$lib/components/ui/input/input.svelte";
-  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
   import { loadVideoConsoles, videoConsole } from "$lib/store/video-consoles";
   import { createEventDispatcher, onMount } from "svelte";
   import { writable } from "svelte/store";
@@ -43,104 +42,4 @@
       <p>No hay registros</p>
     </div>
   {/if}
-  <!-- <div class="rounded-md border" in:fade>
-    <Table.Root {...$tableAttrs}>
-      <Table.Header>
-        {#each $headerRows as headerRow}
-          <Subscribe rowAttrs={headerRow.attrs()}>
-            <Table.Row>
-              {#each headerRow.cells as cell (cell.id)}
-                <Subscribe
-                  attrs={cell.attrs()}
-                  let:attrs
-                  props={cell.props()}
-                  let:props
-                >
-                  <Table.Head
-                    {...attrs}
-                    class={cn("[&:has([role=checkbox])]:pl-3")}
-                  >
-                    {#if cell.id === "amount"}
-                      <div class="text-right font-medium">
-                        <Render of={cell.render()} />
-                      </div>
-                    {:else if cell.id === "email"}
-                      <Button variant="ghost" on:click={props.sort.toggle}>
-                        <Render of={cell.render()} />
-                        <div
-                          class={cn(
-                            "i-ph-arrows-down-up ml-2 h-4 w-4",
-                            $sortKeys[0]?.id === cell.id && "text-foreground"
-                          )}
-                        />
-                      </Button>
-                    {:else}
-                      <Render of={cell.render()} />
-                    {/if}
-                  </Table.Head>
-                </Subscribe>
-              {/each}
-            </Table.Row>
-          </Subscribe>
-        {/each}
-      </Table.Header>
-      <Table.Body {...$tableBodyAttrs}>
-        {#if $pageRows.length}
-          {#each $pageRows as row (row.id)}
-            <Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-              <Table.Row
-                {...rowAttrs}
-                data-state={$selectedDataIds[row.id] && "selected"}
-              >
-                {#each row.cells as cell (cell.id)}
-                  <Subscribe attrs={cell.attrs()} let:attrs>
-                    <Table.Cell
-                      class="[&:has([role=checkbox])]:pl-3"
-                      {...attrs}
-                    >
-                      {#if cell.id === "amount"}
-                        <div class="text-right font-medium">
-                          <Render of={cell.render()} />
-                        </div>
-                      {:else if cell.id === "status"}
-                        <div class="capitalize">
-                          <Render of={cell.render()} />
-                        </div>
-                      {:else}
-                        <Render of={cell.render()} />
-                      {/if}
-                    </Table.Cell>
-                  </Subscribe>
-                {/each}
-              </Table.Row>
-            </Subscribe>
-          {/each}
-        {:else}
-          <Table.Row>
-            <Table.Cell colspan={5}>
-              <p class="text-center">No hay registros</p>
-            </Table.Cell>
-          </Table.Row>
-        {/if}
-      </Table.Body>
-    </Table.Root>
-  </div>
-  <div class="flex items-center justify-end space-x-2 py-4">
-    <div class="flex-1 text-sm text-muted-foreground">
-      {Object.keys($selectedDataIds).length} de{" "}
-      {$rows.length} fila(s) seleccionadas.
-    </div>
-    <Button
-      variant="outline"
-      size="sm"
-      on:click={() => ($pageIndex = $pageIndex - 1)}
-      disabled={!$hasPreviousPage}>Anterior</Button
-    >
-    <Button
-      variant="outline"
-      size="sm"
-      disabled={!$hasNextPage}
-      on:click={() => ($pageIndex = $pageIndex + 1)}>Siguiente</Button
-    >
-  </div> -->
 </div>
